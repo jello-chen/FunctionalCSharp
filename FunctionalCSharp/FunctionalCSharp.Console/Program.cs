743e7ac7ce5c8741bc9380821b9e42c8a4154897
jello chen <jello_chen@live.com>
2018-03-17 01:09:54 +0800

    add combinator

--- FunctionalCSharp/FunctionalCSharp.Console/Program.cs
+++ FunctionalCSharp/FunctionalCSharp.Console/Program.cs
@@ -6,6 +6,7 @@ namespace FunctionalCSharp.Console
     {
         static void Main(string[] args)
         {
+            // Combinator
             Optional<Calculator> c1 = new Calculator { First = 1, Second = 1 };
             Optional<Calculator> c2 = new Calculator { First = 2, Second = 2 };
             Optional<Calculator> c3 = new Calculator { First = 3, Second = 3 };
@@ -15,6 +16,14 @@ namespace FunctionalCSharp.Console
                     from k in c3
                     select (i.First + j.First + k.First) * (i.Second + j.Second + k.Second) + "";
             System.Console.WriteLine(q.Return());
+
+            // Fixed Combinator
+            var fac = Combinator.Fix<int, int>(f => x => x <= 1 ? 1 : x * f(x - 1));
+            var fib = Combinator.Fix<int, int>(f => x => x <= 1 ? 1 : f(x - 1) + f(x - 2));
+            var gcd = Combinator.Fix<int, int, int>(f => (x, y) => y == 0 ? x : f(y, x % y));
+            System.Console.WriteLine(fac(5));
+            System.Console.WriteLine(fib(5));
+            System.Console.WriteLine(gcd(9 , 3));
 
             System.Console.ReadKey();
         }
---
+++ FunctionalCSharp/FunctionalCSharp.Core/Combinator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FunctionalCSharp.Core
+{
+    /// <summary>
+    /// Combinator
+    /// </summary>
+    public static class Combinator
+    {
+        #region Y Combinator
+        public static Func<T, TResult> Fix<T, TResult>(Func<Func<T, TResult>, Func<T, TResult>> f) => x => f(Fix(f))(x);
+
+        public static Func<T1, T2, TResult> Fix<T1, T2, TResult>(Func<Func<T1, T2, TResult>, Func<T1, T2, TResult>> f) => (x, y) => f(Fix(f))(x, y);
+        #endregion
+
+
+        #region SKI Combinator
+        public static Func<Func<TResult, TResult>, Func<Func<T, TResult>, T>, Func<T, TResult>, TResult> S<T, TResult>() => (x, y, z) => x(z(y(z)));
+
+        public static Func<T1, Func<T2, T1>> K<T1, T2>() => x => y => x;
+
+        public static Func<T, T> I<T>() => x => x; 
+        #endregion
+    }
+}
